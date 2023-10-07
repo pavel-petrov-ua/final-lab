822c3d3eba837587c449370507532f9283494188
Pavlo, Petrov <pavel_petrov@ukr.net>
2023-10-07 14:51:35 +0300

    changes

--- terraform/vpc.tf
+++ terraform/vpc.tf
@@ -16,7 +16,7 @@ resource "aws_default_network_acl" "default" {
 
   egress {
     protocol   = -1
-    rule_no    = 100s
+    rule_no    = 100
     action     = "allow"
     cidr_block = "0.0.0.0/0"
     from_port  = 0
